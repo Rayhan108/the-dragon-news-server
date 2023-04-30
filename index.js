652babd17b5b1cb5075dd5b27bcd0d29025955d1
Rayhan Shorker <raihanshorker56@gmail.com>
2023-04-30 23:18:57 +0600

    added vercel confiq file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const cors =require('cors');
 const catagories = require('./data/catagories.json');
+const news = require('./data/news.json')
 const app =express();
 const port =process.env.PORT || 5000;
 
@@ -13,6 +14,29 @@ app.use(cors());
 app.get('/catagories',(req,res)=>{
     res.send(catagories)
 })
+app.get('/news',(req,res)=>{
+    res.send(news);
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id =req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n =>n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/catagories/:id',(req,res)=>{
+    const id =parseInt(req.params.id);
+    console.log(id);
+if(id === 0){
+    res.send(news)
+}else{
+    const catagoryNews = news.filter(n =>parseInt(n.category_id) === id)
+    res.send(catagoryNews)
+}
+
+})
+
 app.listen(port,()=>{
     console.log(`dragon api is runing on port:${port}`);
 })
